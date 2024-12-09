8a87e727329b586cafd67706037b9ff64929b24f
Gea1926 <l.lanzetta6@studenti.unisa.it>
2024-12-09 15:59:45 +0100

    Revert "Develop"

--- rubrica/src/main/java/it/unisa/diem/ingsoftw/gruppo16/controller/AddandModifyController.java
+++ rubrica/src/main/java/it/unisa/diem/ingsoftw/gruppo16/controller/AddandModifyController.java
@@ -88,7 +88,7 @@ public class AddandModifyController implements Initializable{
     }
     @FXML 
     private void delContactOnAction(ActionEvent event){
-        Alert alert = new Alert(AlertType.CONFIRMATION); 
+        Alert alert = new Alert(AlertType.WARNING); 
         alert.setTitle("Conferma Azione"); 
         alert.setHeaderText("Sei sicuro di voler procedere?"); 
         alert.setContentText("Questa azione non può essere annullata."); 
@@ -102,25 +102,10 @@ public class AddandModifyController implements Initializable{
     }
     @FXML
     private void cancelOnAction(ActionEvent event) throws IOException{
-        switchSceneToDashboard(event);
+        Parent scene2Root = FXMLLoader.load(getClass().getResource("interface.fxml")); 
+        Stage stage = (Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow(); 
+        Scene scene2 = new Scene(scene2Root); 
+        stage.setScene(scene2); 
+        stage.show();
     }
-    @FXML
-    private void saveBtnOnAction(ActionEvent event) throws IOException{
-        switchSceneToDashboard(event);
-    }
-
-
-
-    private void switchSceneToDashboard(ActionEvent event) throws IOException{
-        try {
-            Parent scene2Root = FXMLLoader.load(getClass().getResource("/it/unisa/diem/ingsoftw/gruppo16/fxmlDir/interface.fxml")); 
-            Stage stage = (Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow(); 
-            Scene scene2 = new Scene(scene2Root); 
-            stage.setScene(scene2); 
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
 }
