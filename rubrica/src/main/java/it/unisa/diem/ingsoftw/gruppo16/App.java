798f47d75f7f2b5520af2e46d8407d52c5d84b9d
Maxim Makhovskyy <m.makhovskyy@studenti.unisa.it>
2024-12-09 17:30:48 +0100

    Risolto problema dell'aggiornamento del merge

--- rubrica/src/main/java/it/unisa/diem/ingsoftw/gruppo16/App.java
+++ rubrica/src/main/java/it/unisa/diem/ingsoftw/gruppo16/App.java
@@ -1,13 +1,38 @@
 package it.unisa.diem.ingsoftw.gruppo16;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    private static Scene scene;
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        launch(args);
+    }
+
+     @Override
+    public void start(Stage stage){
+        try {
+            Parent root = loadFXML("fxmlDir/interface");
+            Scene scene1 = new Scene(root);
+            stage.setScene(scene1);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
     }
 }
