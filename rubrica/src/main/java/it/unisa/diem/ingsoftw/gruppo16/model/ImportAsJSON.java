6a0b32aa0d8e8bcb373fc289da7cf24899d83d20
Luca Lanzetta <l.lanzetta6@studenti.unisa.it>
2024-12-10 13:15:14 +0100

    Implementati i metodi importFile

--- rubrica/src/main/java/it/unisa/diem/ingsoftw/gruppo16/model/ImportAsJSON.java
+++ rubrica/src/main/java/it/unisa/diem/ingsoftw/gruppo16/model/ImportAsJSON.java
@@ -1,22 +1,56 @@
 package it.unisa.diem.ingsoftw.gruppo16.model;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.TreeSet;
+
 /**
- * @Class ImportAsJSON
- * @brief Strategia per importare i contatti in un file in formato JSON
- * @verison 1.0
- */
+ 
+@Class ImportAsJSON
+@brief Strategia per importare i contatti in un file in formato JSON
+@version 1.0*/
 public class ImportAsJSON implements ImportFileStrategy {
-        /**
-     * 
-     * @param[in] filename Path del file da importare
-     * @return TreeSet con i contatti importati dal file JSON.
-     */
     @Override
     public TreeSet<Contact> importFile(String filename) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'importFileAs'");
-    }
+        TreeSet<Contact> contacts = new TreeSet<>();
+        try {
 
-    
+            String content = new String(Files.readAllBytes(Paths.get(filename)));
+            JSONArray jsonArray = new JSONArray(content);
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                String name = jsonObject.optString("name", "");
+                String surname = jsonObject.optString("surname", "");
+
+
+                if (name.isEmpty() && surname.isEmpty()) {
+                    continue;
+                }
+
+                Contact contact = new Contact(name, surname);
+
+
+                if (jsonObject.has("phoneNumbers")) {
+                    String[] phoneNumbers = jsonObject.getString("phoneNumbers").split(";");
+                    contact.setTelephoneNumber(phoneNumbers);
+                }
+
+                if (jsonObject.has("emails")) {
+                    String[] emails = jsonObject.getString("emails").split(";");
+                    contact.setEmail(emails);
+                }
+
+                contacts.add(contact);
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return contacts;
+    }
 }
--- rubrica/src/main/java/it/unisa/diem/ingsoftw/gruppo16/model/ImportAsXML.java
+++ rubrica/src/main/java/it/unisa/diem/ingsoftw/gruppo16/model/ImportAsXML.java
@@ -1,20 +1,59 @@
 package it.unisa.diem.ingsoftw.gruppo16.model;
 import java.util.TreeSet;
-
+import java.io.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
 /**
  * @Class ImportAsXML
  * @brief Strategia per importare i contatti in un file in formato XML
  * @verison 1.0
  */
+
+
 public class ImportAsXML implements ImportFileStrategy {
-    /**
-     * 
-     * @param[in] filename Path del file da importare
-     * @return TreeSet con i contatti importati dal file XML.
-     */
     @Override
     public TreeSet<Contact> importFile(String filename) {
-            return null;
-        
+        TreeSet<Contact> contacts = new TreeSet<>();
+        try {
+            File file = new File(filename);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(file);
+            doc.getDocumentElement().normalize();
+
+            NodeList nodeList = doc.getElementsByTagName("contact");
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+
+                    String name = element.getElementsByTagName("name").item(0).getTextContent();
+                    String surname = element.getElementsByTagName("surname").item(0) != null ? element.getElementsByTagName("surname").item(0).getTextContent() : "";
+
+                    if (name.isEmpty() && surname.isEmpty()) {
+                        continue;
+                    }
+
+                    Contact contact = new Contact(name, surname);
+
+                    if (element.getElementsByTagName("phoneNumbers").item(0) != null) {
+                        String[] phoneNumbers = element.getElementsByTagName("phoneNumbers").item(0).getTextContent().split(";");
+                        contact.setTelephoneNumber(phoneNumbers);
+                    }
+
+                    if (element.getElementsByTagName("emails").item(0) != null) {
+                        String[] emails = element.getElementsByTagName("emails").item(0).getTextContent().split(";");
+                        contact.setEmail(emails);
+                    }
+
+                    
+                    contacts.add(contact);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return contacts;
     }
 }
